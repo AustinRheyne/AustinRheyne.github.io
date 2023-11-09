af0372bb8932d5c07e8cb639571831ed018278da
Austin Rheyne <57242191+AustinRheyne@users.noreply.github.com>
2023-11-08 20:54:45 -0500

    Add files via upload

--- submit.js
+++ submit.js
@@ -1,27 +1,11 @@
-// Information to be mailed to the tester
-const language = document.querySelectorAll("[data-select-language]")[0];
-const teamName = document.querySelectorAll("[data-team-name]")[0];
-const code = document.querySelectorAll("[data-code]")[0];
-const question = document.querySelectorAll("[data-select-question]")[0];
-
 // Submit button
 const submitBtn = document.querySelectorAll("[data-submit]")[0];
 
 
 submitBtn.addEventListener("click", function() {
-    // Get the data from the form
-    let lang = language.value;
-    let team = teamName.value;
-    let codeText = code.value;
-    let q = question.value;
 
     // Email the data in an organized way to the tester
-    emailjs.send("service_pq18mee","template_2rh2ekd",{
-        team_name: team,
-        language: lang,
-        code: codeText,
-        question: q
-        }).then(function (response) {
+    emailjs.sendForm("service_pq18mee","template_2rh2ekd", submitBtn.parentElement).then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 // Send the user back to the questions page
                 window.location.href = "index.html";
@@ -29,6 +13,4 @@ submitBtn.addEventListener("click", function() {
                 // Do nothing since the email failed
                 console.log('FAILED...', error);
         });
-
-    
 });
